207a658b761c90b12f2f97bb153934e0504cf8e1
Konrad Król <44756736+konrad-krol@users.noreply.github.com>
2020-07-03 03:32:40 +0200

    Create Audio_Devices.cs

---
+++ Audio_Settings/Audio_Settings/Audio_Devices.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Audio;
+using Windows.Devices.Enumeration;
+
+namespace Audio_Settings
+{
+    class Audio_Devices
+    {
+
+        private static Dictionary<string, string> audioDevices = new Dictionary<string, string>();
+
+        private static SpatialAudioDeviceConfiguration SpatialAudioConfiguration { get; set; }
+
+        public async static void FindAudioDevices()
+        {
+            audioDevices.Clear();
+            var getDevices = await DeviceInformation.FindAllAsync(DeviceClass.AudioRender);
+            foreach (var getDevice in getDevices)
+            {
+                audioDevices.Add(getDevice.Id, getDevice.Name);
+            }
+            
+        }
+        public static Dictionary<string, string> GetAudioDevices()
+        {
+            return audioDevices;
+        }
+        public static Dictionary<string, string> GetDefaultDevice()
+        {
+            return Audio_Default.FindDefaultDevice(audioDevices);
+        }
+
+
+    }
+
+}
